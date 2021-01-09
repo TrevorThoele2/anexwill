5fe6937c601672f60dadbd675d29523afa425aaa
TrevorThoele <t_thoele@yahoo.com>
2021-01-09 09:34:38 -0600

    Remove log.txt.

--- AnExWill/Main.cpp
+++ AnExWill/Main.cpp
@@ -1,27 +1,19 @@
 #include <locale>
-#include <codecvt>
 
-#include <Atmos/WindowsEngine.h>
+#include <Atmos/SDLEngine.h>
 #include <Atmos/AssetsFileExtension.h>
 #include <Atmos/Logger.h>
+#include <SDL.h>
 
-std::optional<Atmos::File::Path> WorldPath()
+std::optional<Atmos::File::Path> WorldPath(int argc, char* argv[])
 {
-    int argCount;
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-
-    const auto szArgList = CommandLineToArgvW(GetCommandLineW(), &argCount);
-    if (szArgList == nullptr || argCount < 2)
+    if (argc < 2 || argv == nullptr)
         return {};
 
-    auto returnValue = std::string(converter.to_bytes(std::wstring(szArgList[1])));
-
-    LocalFree(szArgList);
-
-    return returnValue;
+    return argv[1];
 }
 
-void StartEngineExecution(Atmos::WindowsEngine& engine, const Atmos::File::Path& worldPath)
+void StartEngineExecution(Atmos::SDLEngine& engine, const Atmos::File::Path& worldPath)
 {
     auto assetsPath = worldPath;
     assetsPath.replace_extension(Atmos::World::Serialization::assetsFileExtension);
@@ -31,34 +23,36 @@ void StartEngineExecution(Atmos::WindowsEngine& engine, const Atmos::File::Path&
     engine.StartExecution();
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int main(int argc, char* argv[])
 {
     Atmos::Logging::Logger logger(Atmos::Logging::Severity::Verbose);
 
     try
     {
-        auto worldPath = WorldPath();
+        auto worldPath = WorldPath(argc, argv);
 
         if (!worldPath)
         {
-            MessageBox(nullptr, L"A world file needs to be supplied.", L"Error", MB_OK);
-            return 0;
+            SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "A world file needs to be supplied.", nullptr);
+            return -3;
         }
 
-        Atmos::WindowsEngine engine(hInstance, lpCmdLine, nCmdShow, "AnExWill", logger);
+        Atmos::SDLEngine engine(logger);
         StartEngineExecution(engine, *worldPath);
     }
-    catch (const std::exception & e)
+    catch (const std::exception& e)
     {
         logger.Log(
             e.what(),
             Atmos::Logging::Severity::Error);
+        return -2;
     }
-    catch(...)
+    catch (...)
     {
         logger.Log(
             "Unknown error.",
             Atmos::Logging::Severity::Error);
+        return -1;
     }
 
     return 0;
